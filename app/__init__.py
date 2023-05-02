2d029b47b6ce5a2572e711728a9632a5910bedd1
Melinda Mott <melinda.mott@gmail.com>
2023-05-02 11:43:30 -0700

    dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os 
 
 db = SQLAlchemy()
 migrate = Migrate()
