4e3fbcc1f0547a4f52f5bc46c850508d9dd5f44f
Angie <caritocon@gmail.com>
2023-05-03 16:38:41 -0700

    Typo in app __init__ fixed

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
-import os 
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -13,11 +13,11 @@ def create_app(test_config=None):
 
     if not test_config:
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        app.config["SQUALCHEMY_DATABASE_URI"] = os.environ.get('SQUALCHEMY_DATABASE_URI')
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     app.config["TESTING"] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQUALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_TEST_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     db.init_app(app)
     migrate.init_app(app, db)
